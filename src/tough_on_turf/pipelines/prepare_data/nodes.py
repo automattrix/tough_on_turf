3f8348de447e6b2de299b39f415fd36532d0d6b5
automattrix <matt.karan93@gmail.com>
2020-02-09 22:53:17 -0800

    add full relative path for player injury csv files

--- src/tough_on_turf/pipelines/prepare_data/nodes.py
+++ src/tough_on_turf/pipelines/prepare_data/nodes.py
@@ -2,7 +2,8 @@ import os
 
 
 def _list_files(in_path, directory):
-    csv_files = os.listdir(directory)
+    tmp_csv_files = os.listdir(directory)
+    csv_files = [f'{in_path}{csv_file}' for csv_file in tmp_csv_files]
     return csv_files
 
 
@@ -15,7 +16,7 @@ def _write_csv(csv_list, csv_out):
 
 def preprocess_player_files(p_params):
     print("aggregating player data")
-    lof = _list_files(directory=f'{p_params["data_path_in"]}/{p_params["bodypart"]}')
+    lof = _list_files(in_path=p_params["data_path_in"], directory=f'{p_params["data_path_in"]}/{p_params["bodypart"]}')
     output_path = f'{p_params["data_path_out"]}/{p_params["bodypart"]}_list.csv'
     print(output_path)
     _write_csv(csv_list=lof, csv_out=output_path)
