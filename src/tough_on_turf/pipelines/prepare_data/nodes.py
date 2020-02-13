6c2999f0fd294656c40f73a5ecc289c5ea61792e
automattrix <matt.karan93@gmail.com>
2020-02-12 23:12:16 -0800

    add h5 generation nodes

--- src/tough_on_turf/pipelines/prepare_data/nodes.py
+++ src/tough_on_turf/pipelines/prepare_data/nodes.py
@@ -1,11 +1,11 @@
 import os
-
+import pandas as pd
+import h5py
 
 def _list_files(in_path, directory):
     tmp_csv_files = os.listdir(directory)
     csv_files = [f'{in_path}{csv_file}' for csv_file in tmp_csv_files]
     return csv_files
-
 
 def _write_csv(csv_list, csv_out):
     tmp_out = open(csv_out, 'w')
@@ -14,10 +14,42 @@ def _write_csv(csv_list, csv_out):
     tmp_out.close()
 
 
-def preprocess_player_files(p_params):
+def preprocess_player_files(params):
     print("aggregating player data")
-    lof = _list_files(in_path=p_params["data_path_in"], directory=f'{p_params["data_path_in"]}/{p_params["bodypart"]}')
-    output_path = f'{p_params["data_path_out"]}/{p_params["bodypart"]}_list.csv'
+    lof = _list_files(in_path=params["data_path_in"], directory=f'{params["data_path_in"]}/{params["bodypart"]}')
+    output_path = f'{params["data_path_out"]}/{params["bodypart"]}_list.csv'
     print(output_path)
     _write_csv(csv_list=lof, csv_out=output_path)
     return output_path
+
+
+def _extract_playerkey(input_playkey):
+    try:
+        tmp_key = str(input_playkey).split('-')
+        player_key = tmp_key[0]
+    except:
+        player_key = None
+    return player_key
+
+def generate_h5(params):
+
+    if params["run_h5"]:
+        # TODO change this file check behaviour. Method to be set in params (overwrite, move, delete, etc)
+        if os.path.exists(params["data_path_out"]):
+            exit()
+        else:
+            for chunk in pd.read_csv(params["data_path_in"], chunksize=params["chunksize"]):
+                chunk['PlayerKey'] = chunk['PlayKey'].apply(_extract_playerkey)
+                # print(chunk)
+                unique_players = chunk['PlayerKey'].unique()
+                print(unique_players)
+                for player in unique_players:
+                    keyname = f'player_{player}'
+                    # print(keyname)
+                    df = chunk.loc[chunk['PlayerKey'] == player]
+                    # print(df)
+                    df.to_hdf('./data/02_intermediate/nfl_trackdata.h5', append=True,
+                              key=keyname, min_itemsize=100, complevel=params["compression_level"])
+
+
+
--- src/tough_on_turf/pipelines/prepare_data/pipeline.py
+++ src/tough_on_turf/pipelines/prepare_data/pipeline.py
@@ -1,10 +1,16 @@
 from kedro.pipeline import Pipeline, node
-from .nodes import preprocess_player_files
+from .nodes import preprocess_player_files, generate_h5
 
 
 def create_pipeline(**kwargs):
     return Pipeline(
         [
+            node(
+                func=generate_h5,
+                inputs=["params:data_prep"],
+                outputs="csv_path",
+                name="list_player_csvs",
+            ),
             node(
                 func=preprocess_player_files,
                 inputs=["params:players_injury"],
