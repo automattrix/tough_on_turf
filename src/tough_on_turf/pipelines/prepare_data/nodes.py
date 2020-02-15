98448fd303c4060250e475c7d20244df7f8b9f80
automattrix <matt.karan93@gmail.com>
2020-02-15 14:22:54 -0800

    reorganized functions in prepare_data nodes.py

--- src/tough_on_turf/pipelines/prepare_data/nodes.py
+++ src/tough_on_turf/pipelines/prepare_data/nodes.py
@@ -4,28 +4,6 @@ import h5py
 import contextlib
 
 
-def _list_files(in_path, directory):
-    tmp_csv_files = os.listdir(directory)
-    csv_files = [f'{in_path}{csv_file}' for csv_file in tmp_csv_files]
-    return csv_files
-
-
-def _write_csv(csv_list, csv_out):
-    tmp_out = open(csv_out, 'w')
-    for i in csv_list:
-        tmp_out.write(f'{i},\n')
-    tmp_out.close()
-
-
-def preprocess_player_files(params):
-    print("aggregating player data")
-    lof = _list_files(in_path=params["data_path_in"], directory=f'{params["data_path_in"]}/{params["bodypart"]}')
-    output_path = f'{params["data_path_out"]}/{params["bodypart"]}_list.csv'
-    print(output_path)
-    _write_csv(csv_list=lof, csv_out=output_path)
-    return output_path
-
-
 def _extract_playerkey(input_playkey):
     try:
         tmp_key = str(input_playkey).split('-')
@@ -60,3 +38,25 @@ def generate_h5(params):
 
     else:
         print("Not generating new HDF output file")
+
+
+def _list_files(in_path, directory):
+    tmp_csv_files = os.listdir(directory)
+    csv_files = [f'{in_path}{csv_file}' for csv_file in tmp_csv_files]
+    return csv_files
+
+
+def _write_csv(csv_list, csv_out):
+    tmp_out = open(csv_out, 'w')
+    for i in csv_list:
+        tmp_out.write(f'{i},\n')
+    tmp_out.close()
+
+
+def preprocess_player_files(params):
+    print("aggregating player data")
+    lof = _list_files(in_path=params["data_path_in"], directory=f'{params["data_path_in"]}/{params["bodypart"]}')
+    output_path = f'{params["data_path_out"]}/{params["bodypart"]}_list.csv'
+    print(output_path)
+    _write_csv(csv_list=lof, csv_out=output_path)
+    return output_path
