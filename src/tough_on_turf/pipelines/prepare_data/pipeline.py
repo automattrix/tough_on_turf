b0a28cbe256074c7b8955cba9b04004753d8108d
automattrix <matt.karan93@gmail.com>
2020-02-16 12:06:02 -0800

    renamed preprocess_player_files node to list_player_csvs. Added generate_custom_csv to pipeline

--- src/tough_on_turf/pipelines/prepare_data/pipeline.py
+++ src/tough_on_turf/pipelines/prepare_data/pipeline.py
@@ -1,5 +1,5 @@
 from kedro.pipeline import Pipeline, node
-from .nodes import preprocess_player_files, generate_h5
+from .nodes import list_player_csvs, generate_h5, prep_injury_data, generate_custom_csv
 
 
 def create_pipeline(**kwargs):
@@ -12,11 +12,24 @@ def create_pipeline(**kwargs):
                 name="generate_h5",
             ),
             node(
-                func=preprocess_player_files,
+                func=prep_injury_data,  # writes to './data/02_intermediate/bodypart/bodypart_injury.csv'
+                inputs=["injury_record", "params:prep_injury"],
+                outputs="body_injury",
+                name="prep_injury_data",
+            ),
+            node(
+                func=generate_custom_csv,  # from bodypart_injury
+                inputs=["injury_record", "params:generate_custom_csv"],
+                outputs=None,
+                name="generate_custom_csv",
+            ),
+            node(
+                func=list_player_csvs,
                 inputs=["params:players_injury"],
                 outputs="csv_path",
                 name="list_player_csvs",
             ),
+
         ]
     )
 
