24e1dc19f563501772a4819cfb2e56326896130f
automattrix <matt.karan93@gmail.com>
2020-03-08 17:32:54 -0700

    rename bodypart_df_list function add calc_metrics node

--- src/tough_on_turf/pipelines/group_metrics/pipeline.py
+++ src/tough_on_turf/pipelines/group_metrics/pipeline.py
@@ -1,15 +1,21 @@
 from kedro.pipeline import Pipeline, node
-from .nodes import calc_speed
+from .nodes import create_bodypart_df_list, calc_metrics
 
 
 def create_pipeline(**kwargs):
     return Pipeline(
         [
             node(
-                func=calc_speed,
-                inputs="companies",
-                outputs="preprocessed_companies",
-                name="preprocessing_companies",
+                func=create_bodypart_df_list,
+                inputs="csv_paths_list",
+                outputs="df_list",
+                name="bodypart_df_list",
+            ),
+            node(
+                func=calc_metrics,
+                inputs=["csv_paths_list", "params:calc_metrics"],
+                outputs="o_dir_df_list",
+                name="calc_metrics",
             ),
         ]
     )
